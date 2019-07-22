868a1ad15134be239c4aa149b7fd3ddbcfb6421a
gerritt <gmocore@gmail.com>
2019-07-22 11:38:09 -0600

    added objects for each player

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,13 @@
     // base attack
     // attack
     // counter attack
+    let health;
+    let baseAttack;
+    let attack;
+    let counterAttack
+    let selectedPlayer
+    let selectedOpponent
+
     const leo = {
         health: 100,
         baseAttack: 10,
@@ -28,7 +35,8 @@
         attack: 0,
         counterAttack: 8,
         selectedPlayer: false,
-        selectedOpponent: false
+        selectedOpponent: false,
+        imgUrl: './assets/images/screen-shot-2019-03-12-at-164638_khxp.910.webp'
     }
     const aubrey = {
         health: 80,
@@ -39,11 +47,40 @@
         selectedOpponent: false
     }
 
+    $('#idris').attr('src', idris.imgUrl)
+
     const characters = document.querySelectorAll('.character')
 
+
     characters.forEach(item => {
+        console.log(this);
+        
         item.addEventListener('click', () => {
-           console.log(item.id);
+           if(item.id === "aubrey") {
+               health = aubrey.health;
+               baseAttack = aubrey.baseAttack;
+               attack = aubrey.attack;
+               counterAttack = aubrey.counterAttack;
+            console.log(health, baseAttack, attack, counterAttack)
+           } else if(item.id === 'leo') {
+            health = leo.health;
+            baseAttack = leo.baseAttack;
+            attack = leo.attack;
+            counterAttack = leo.counterAttack;
+         console.log(health, baseAttack, attack, counterAttack)
+           } else if (item.id === 'brad') {
+            health = brad.health;
+            baseAttack = brad.baseAttack;
+            attack = brad.attack;
+            counterAttack = brad.counterAttack;
+         console.log(health, baseAttack, attack, counterAttack)
+        //    } else if(item.id === 'idris') {
+        //     health = idris.health;
+        //     baseAttack = idris.baseAttack;
+        //     attack = idris.attack;
+        //     counterAttack = idris.counterAttack;
+        //  console.log(health, baseAttack, attack, counterAttack)
+           }
            
         })
     })
