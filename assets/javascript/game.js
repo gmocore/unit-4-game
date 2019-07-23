676f5ca7e38f8268effb46f776ceaabf9145a7c3
Black <gmocore@gmail.com>
2019-07-23 13:26:28 -0600

    refactored click handler. click moves characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,93 @@
 //create object for each character
-    // image
-    // health
-    // base attack
-    // attack
-    // counter attack
-    let health;
-    let baseAttack;
-    let attack;
-    let counterAttack
-    let selectedPlayer
-    let selectedOpponent
+// image
+// health
+// base attack
+// attack
+// counter attack
+let playerSelected;
 
-    const leo = {
-        health: 100,
-        baseAttack: 10,
-        attack: 0,
-        counterAttack: 5,
-        selectedPlayer: false,
-        selectedOpponent: false,
-        id: 'leo'
+const characters = {
+  leo: {
+    name: "leo",
+    health: 100,
+    baseAttack: 10,
+    attack: 0,
+    counterAttack: 5,
+    selectedPlayer: false,
+    selectedOpponent: true
+  },
+  brad: {
+    name: "brad",
+    health: 125,
+    baseAttack: 15,
+    attack: 0,
+    counterAttack: 10,
+    selectedPlayer: false,
+    selectedOpponent: true
+  },
+  idris: {
+    name: "idris",
+    health: 140,
+    baseAttack: 8,
+    attack: 0,
+    counterAttack: 8,
+    selectedPlayer: false,
+    selectedOpponent: true
+  },
+  aubrey: {
+    name: "aubrey",
+    health: 80,
+    baseAttack: 25,
+    attack: 0,
+    counterAttack: 15,
+    selectedPlayer: false,
+    selectedOpponent: true
+  }
+};
+
+const characterImages = document.querySelectorAll(".character");
+
+$(".character").on("click", function() {
+  playerSelected = $(this).attr("id");
+
+  characterImages.forEach(item => {
+    if (item.id !== playerSelected) {
+      $(item).appendTo(".opponent");
+    } else {
+      $(item).appendTo(".selected-player");
     }
-    
-    const brad = {
-        health: 125,
-        baseAttack: 15,
-        attack: 0,
-        counterAttack: 10,
-        selectedPlayer: false,
-        selectedOpponent: false
-    }
-    const idris = {
-        health: 140,
-        baseAttack: 8,
-        attack: 0,
-        counterAttack: 8,
-        selectedPlayer: false,
-        selectedOpponent: false,
-        imgUrl: './assets/images/screen-shot-2019-03-12-at-164638_khxp.910.webp'
-    }
-    const aubrey = {
-        health: 80,
-        baseAttack: 25,
-        attack: 0,
-        counterAttack: 15,
-        selectedPlayer: false,
-        selectedOpponent: false
-    }
+  });
+});
 
-    $('#idris').attr('src', idris.imgUrl)
+$(".attack-button").click(event => {
+  console.log("attack");
+});
 
-    const characters = document.querySelectorAll('.character')
-
-
-    characters.forEach(item => {
-        console.log(this);
-        
-        item.addEventListener('click', () => {
-           if(item.id === "aubrey") {
-               health = aubrey.health;
-               baseAttack = aubrey.baseAttack;
-               attack = aubrey.attack;
-               counterAttack = aubrey.counterAttack;
-            console.log(health, baseAttack, attack, counterAttack)
-           } else if(item.id === 'leo') {
-            health = leo.health;
-            baseAttack = leo.baseAttack;
-            attack = leo.attack;
-            counterAttack = leo.counterAttack;
-         console.log(health, baseAttack, attack, counterAttack)
-           } else if (item.id === 'brad') {
-            health = brad.health;
-            baseAttack = brad.baseAttack;
-            attack = brad.attack;
-            counterAttack = brad.counterAttack;
-         console.log(health, baseAttack, attack, counterAttack)
-        //    } else if(item.id === 'idris') {
-        //     health = idris.health;
-        //     baseAttack = idris.baseAttack;
-        //     attack = idris.attack;
-        //     counterAttack = idris.counterAttack;
-        //  console.log(health, baseAttack, attack, counterAttack)
-           }
-           
-        })
-    })
+$(".reset-button").click(event => {
+  console.log("reset");
+});
 
 // create event listener for:
-    // select character (player)
-    // select opponent
-    // attack
+// select character (player)
+// select opponent
+// attack
 
 // when player is selected
-    // image is moved to fight area
+// image is moved to fight area
 
-// when opponent is selected 
-    // opponent moved to defender area
+// when opponent is selected
+// opponent moved to defender area
 
 // character attack
-    // each attack increments attack by base attack
-    // attack decrements hp of oponent by attack value
+// each attack increments attack by base attack
+// attack decrements hp of oponent by attack value
 
 // opponent attack
-    // character attack tigeers counter attack
-    // counter decrements player hp
+// character attack tigeers counter attack
+// counter decrements player hp
 
-// health 
-    // if hp is <= 0 player/opponent defeated
-    // if player is defeated => game over
-    // if opponent is defeated => select new opponent
-        // if no opponents remain => player wins
+// health
+// if hp is <= 0 player/opponent defeated
+// if player is defeated => game over
+// if opponent is defeated => select new opponent
+// if no opponents remain => player wins
