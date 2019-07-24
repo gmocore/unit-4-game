9efe1b7578e507b1ca54e046d0357a2e8cd35fbd
gerritt <gmocore@gmail.com>
2019-07-24 09:24:36 -0600

    added logic to select player and opponent. attack and counter functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,50 +5,65 @@
 // attack
 // counter attack
 let playerSelected;
+let playerBoolean = false;
+let opponentSelected;
+let opponentBoolean = false;
 
 const characters = {
   leo: {
     name: "leo",
     health: 100,
     baseAttack: 10,
-    attack: 0,
+    attack: 10,
     counterAttack: 5,
     selectedPlayer: false,
-    selectedOpponent: true
+    selectedOpponent: false
   },
   brad: {
     name: "brad",
     health: 125,
     baseAttack: 15,
-    attack: 0,
+    attack: 15,
     counterAttack: 10,
     selectedPlayer: false,
-    selectedOpponent: true
+    selectedOpponent: false
   },
   idris: {
     name: "idris",
     health: 140,
     baseAttack: 8,
-    attack: 0,
+    attack: 8,
     counterAttack: 8,
     selectedPlayer: false,
-    selectedOpponent: true
+    selectedOpponent: false
   },
   aubrey: {
     name: "aubrey",
     health: 80,
     baseAttack: 25,
-    attack: 0,
+    attack: 25,
     counterAttack: 15,
     selectedPlayer: false,
-    selectedOpponent: true
+    selectedOpponent: false
   }
 };
 
 const characterImages = document.querySelectorAll(".character");
 
 $(".character").on("click", function() {
-  playerSelected = $(this).attr("id");
+  if(!playerSelected && !opponentSelected) {
+    playerSelected = $(this).attr("id");
+    playerBoolean = true;
+    console.log(characters[playerSelected].baseAttack, playerBoolean)
+
+  }
+  
+  else if (playerBoolean && !opponentBoolean) {
+    opponentSelected = $(this).attr('id')
+    opponentBoolean = true
+    console.log(characters[opponentSelected].counterAttack, opponentBoolean);
+    
+  }
 
   characterImages.forEach(item => {
     if (item.id !== playerSelected) {
@@ -61,11 +76,26 @@ $(".character").on("click", function() {
 
 $(".attack-button").click(event => {
   console.log("attack");
+  if (playerSelected && opponentSelected) {
+    playerAttack();
+    opponentCounter()
+  }
 });
 
 $(".reset-button").click(event => {
   console.log("reset");
 });
+
+
+function playerAttack() {
+  characters[opponentSelected].health -= characters[playerSelected].baseAttack;
+  console.log(characters[opponentSelected].health)
+}
+
+function opponentCounter() {
+  characters[playerSelected].health -= characters[opponentSelected].counterAttack;
+  console.log(characters[playerSelected].health)
+}
 
 // create event listener for:
 // select character (player)
